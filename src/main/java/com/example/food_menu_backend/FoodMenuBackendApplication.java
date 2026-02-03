15f71cdfb947882842f929e558c028dcaec357f1
mhmadhfz <apiss2272@gmail.com>
2026-02-03 14:30:40 +0800

    chore: initialize Spring Boot project

---
+++ src/main/java/com/example/food_menu_backend/FoodMenuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_menu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMenuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMenuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/food_menu_backend/FoodMenuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_menu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMenuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
